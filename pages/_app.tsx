a594bef2474a9b34702db4efd699cf16c9cfb5d4
Henrique <henriquedasilvacosta@live.com>
2023-05-05 16:43:21 -0300

    ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Main from "../components/Main";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return (
     <>
       <Main>
